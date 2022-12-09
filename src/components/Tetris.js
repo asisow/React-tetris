ad10df00691046579c99db01ed900d51af82f694
asisow <38430905+asisow@users.noreply.github.com>
2022-12-09 16:27:37 +0300

    Update Tetris.js

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -12,25 +12,59 @@ import StartButton from './StartButton';
 import { useState } from 'react';
 
 const Tetris = ({ type }) => {
-	const [dropTime, setDropTime] = useState(null)
-	const [gameOver, setGameOver] = useState(false)
+	const [ dropTime, setDropTime ] = useState(null);
+	const [ gameOver, setGameOver ] = useState(false);
 
-	const [player] = usePlayer()
-	const [stage, setStage] = useStage(player)
+	const [ player ] = usePlayer();
+	const [ stage, setStage ] = useStage(player);
 
 	console.log('re-render');
 
+	const movePlayer = dir => {
+
+	}
+
+	const startGame = () => {
+		// Reset everything
+		setStage(createStage())
+		// resetPlayer()
+	}
+
+	const drop = () => {
+		// updatePlayerPos({x: 0, y: 1б, collided: false})
+	}
+
+	const dropPlayer = () => {
+		drop()
+	}
+
+	const move = ({keyCode}) => {
+		if (!gameOver) {
+			if (keyCode === 37) {
+				movePlayer(-1)
+			} else if (keyCode === 39) {
+				movePlayer(1)
+			} else if (keyCode = 40) {
+				dropPlayer()
+			}
+		}
+	}
+
 	return (
-		<StyledTetrisWrapper>
+		<StyledTetrisWrapper role='button' tabIndex="0" onKeyDown={e => move(e)}>
 			<StyledTetris>
 				<Stage stage={stage} />
 				<aside>
-					<div>
-						<Display text="Score" />
-						<Display text="Rows" />
-						<Display text="Level" />
-					</div>
-					<StartButton />
+					{gameOver ? (
+						<Display gameOver={gameOver} text="Game Over" />
+					) : (
+						<div>
+							<Display text="Score" />
+							<Display text="Rows" />
+							<Display text="Level" />
+						</div>
+					)}
+					<StartButton onClick={startGame} />
 				</aside>
 			</StyledTetris>
 		</StyledTetrisWrapper>
